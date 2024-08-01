3160de8a3f8b5121e57e81b8428dfa3ee7c2fb99
BESTEASY <zczczc19658@Gmail.com>
2024-08-01 18:07:05 +0800

    0731

--- pythonProject/爬虫0726/52112_com/test/devostock_com/test1.py
+++
@@ -1,3 +0,0 @@
-https://www.devostock.com/members/count.php?type=photo&id=700184&id_parent=182087&server=12
-
-https://www.devostock.com/members/count.php?type=photo&id=700183&id_parent=182086&server=12
--- pythonProject/爬虫0726/alphacoders_com/demo1.py
+++ pythonProject/爬虫0726/alphacoders_com/demo1.py
@@ -3,4 +3,4 @@
 # 差不多10000张，不需筛选
 # https://initiate.alphacoders.com/download/picfiles/514851/jpg
 # 可以苦苦全爬然后筛，也可以精准爬但selenium click抓包+滚轮滚动条加载
-# 滚动条实现滚动还要调用js
+# 滚动条实现滚动还要调用js 参照标签移动 问题不大
