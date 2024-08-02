a403fae8f396df14ed0f4acc1d7331866959c8cb
BESTEASY <zczczc19658@Gmail.com>
2024-08-02 17:05:39 +0800

    0731

--- pythonProject/爬虫0726/alphacoders_com/planD.py
+++ pythonProject/爬虫0726/alphacoders_com/planD.py
@@ -1,4 +1,5 @@
 import time
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,19 +9,24 @@ with open('C:/Users/BESTEASY/Desktop/新建文本文档.txt','rb') as f:
 
 soup = BeautifulSoup(html, 'html.parser')
 
-itemList=soup.findAll('div', class_='item')
+# itemList=soup.findAll('div', class_='item')
+buttonList=soup.findAll('span', class_='button button-download')
 
 # print(divList)
 i=0
-for div in itemList:
-    print(len(itemList))
+for button in buttonList: # 321
     i+=1
+    if i < 321:
+        continue
     try:
-        src = div.find('div').find('div').find('a').get('href')
-        # print(src)
-        id=src.split('/')[-1]
-        # print(id)
-        imgSrc = f'https://initiate.alphacoders.com/download/picfiles/{id}/jpg'
+        text = button.get('onclick')
+        # print(text)
+        id1=text.split("'")[1]
+        id2=text.split(',')[1]
+        id3=text.split("'")[3]
+        # time.sleep(2)
+        imgSrc = f'https://initiate.alphacoders.com/download/{id1}/{id2}/{id3}' # 1 picfiles/  2 images7/
+        print('执行',imgSrc)
         try:
             imgResponse = requests.get(imgSrc)
 
